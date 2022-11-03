c7c5dbb956fe914663f8207d7937c849fcbf58cc
서솔민 <solgo123@gmail.com>
2022-11-03 13:55:27 +0900

    :construction_worker: remove duplicate rollup plugin with vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 const pkg = require('./package.json');
-// cjs 외부 모듈을 es6로 변환하여 사용할 수 있게해줌
-import commonjs from '@rollup/plugin-commonjs';
 // 노드 모듈에서 다른 확장자를 읽어 올 수 있게해줌 우리같은경우 TS를 사용하니까 js 모듈을 사용하려고하면 필요함 근데 우리가 쓰는건 대부분 ts지 않나 싶음
 import resolve from '@rollup/plugin-node-resolve';
 // .json 파일을 esmodule로 변환해서 사용할 수 있게 해줍니다.
@@ -30,11 +28,7 @@ export default defineConfig({
     rollupOptions: {
       // external: 라이브러리에 포함하지 않을 디펜던시를 명시해주세요 여기다 styled-compoenets를 넣어주었더니 사용처에서 styled-components 를 깔지 않으면 사용이 불가했음
       external: [...Object.keys(pkg.peerDependencies)],
-      plugins: [
-        commonjs({ include: 'node_modules/**' }),
-        resolve({ extensions }),
-        json(),
-      ],
+      plugins: [resolve({ extensions }), json()],
     },
   },
 });
